f3aae20f6d32b5b44afd339cdb4dee9d1ae4faf8
Jelle Versele <jelle.versele@internetarchitects.be>
2014-11-13 17:52:33 +0100

    Add test dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,14 @@
 var config = {
 	testDependencies: [
-		'tests/*.js',
+		'test/*.js',
+		'bower_components/jquery/dist/jquery.js',
+		'bower_components/pintxos-inherit/index.js',
+		'bower_components/pintxos-component/index.js',
+		'bower_components/eventEmitter/EventEmitter.js',
+		'bower_components/bezier-easing/bezier-easing.js',
+		'bower_components/pintxos-animation_timeline/index.js',
+		'bower_components/pintxos-scrollable_native/index.js',
+		'bower_components/pintxos-scrollable_ha/index.js',
 		'index.js'
 	]
 };
